5cf6dce05a8da0ecc71edd353981e769276fef40
David Valin <dvalin@redhat.com>
2026-02-09 04:25:53 -0500

    Adding pyantics and updating test_tools.

--- coremark_pro/results_schema.py
+++ coremark_pro/results_schema.py
@@ -4,18 +4,18 @@ import datetime
 from enum import Enum
 
 class testname(Enum):
-    cjpeg-rose7-preset = "cjpeg-rose7-preset"
+    cjpeg_rose7_preset = "cjpeg-rose7-preset"
     core = "core"
-    linear_alg-mid-100x100-sp = "linear_alg-mid-100x100-sp"
-    loops-all-mid-10k-sp = "loops-all-mid-10k-sp"
+    linear_alg_mid_100x100_sp = "linear_alg-mid-100x100-sp"
+    loops_all_mid_10k_sp = "loops-all-mid-10k-sp"
     nnet_test = "nnet_test"
-    parser-125k = "parser-125k"
-    radix2-big-64k = "radix2-big-64k"
-    sha-test = "sha-test"
-    zip-test = "zip-test"
+    parser_125k = "parser-125k"
+    radix2_big_64k = "radix2-big-64k"
+    sha_test = "sha-test"
+    zip_test = "zip-test"
     Score = "Score"
 
-class Coremark_pro_Results(pydantic.BaseModel):
+class Coremark_Pro_Results(pydantic.BaseModel):
     Test: testname
     Multi_iterations: float = pydantic.Field(gt=0, allow_inf_nan=False)
     Single_iterations: float = pydantic.Field(gt=0, allow_inf_nan=False)
